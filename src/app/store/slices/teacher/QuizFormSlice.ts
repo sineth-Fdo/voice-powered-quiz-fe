f9a881f8dc3944a5ac6f9fba451a46849f99b63c
sineth-Fdo <sinethinosh100@gmail.com>
2025-02-22 14:54:58 +0530

    feat(teacher): Create a New Quiz feature added

--- src/app/store/slices/teacher/QuizFormSlice.ts
+++ src/app/store/slices/teacher/QuizFormSlice.ts
@@ -1,39 +1,41 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
+interface FormOneData {
+  title: string;
+  code: string;
+  description: string;
+  password: string;
+}
+
+interface FormTwoData {
+  teacher: string;
+  subject: string;
+  grade: string;
+  batch: string;
+}
 
 interface FormOneI {
-
-    FormOne: {
-        title: string;
-        code: string;
-        description: string;
-        password: string;
-    };
-    FormTwo: {
-        teacher: string;    
-        subject: string;
-        grade: string;
-        batch: string;
-    };
-    setFormOne: (formOne: any) => void;
-    setFormTwo: (formTwo: any) => void;
+  FormOne: FormOneData;
+  FormTwo: FormTwoData;
+  setFormOne: (formOne: FormOneData) => void;
+  setFormTwo: (formTwo: FormTwoData) => void;
 }
 
 const useQuizFormOneStore = create<FormOneI>((set) => ({
-    FormOne: {
-        title: "",
-        code: "",
-        description: "",
-        password: "",
-    },
-    FormTwo: {
-        teacher: "",
-        subject: "",
-        grade: "",
-        batch: "",
-    },
-    setFormOne: (formOne) => set({ FormOne: formOne }),
-    setFormTwo: (formTwo) => set({ FormTwo: formTwo }),
+  FormOne: {
+    title: "",
+    code: "",
+    description: "",
+    password: "",
+  },
+  FormTwo: {
+    teacher: "",
+    subject: "",
+    grade: "",
+    batch: "",
+  },
+  setFormOne: (formOne) => set({ FormOne: formOne }),
+  setFormTwo: (formTwo) => set({ FormTwo: formTwo }),
 }));
 
 export default useQuizFormOneStore;
